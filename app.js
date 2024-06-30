1581f218f468d5dd74ae5581bbbcbba395e28e5b
Hossein Mirzaei <hosinmirzayi90@gmail.com>
2024-06-30 22:05:15 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const swiperSlider = new Swiper('.swiper', {
+
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+    ,
+   
+    
+  
+})
+
+const swiperSliderr = new Swiper('.swiper', {
+
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+    ,
+   
+    
+  
+})
